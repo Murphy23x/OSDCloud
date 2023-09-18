2989e0a86d57397d10b6968de258781ea988aefa
Murphy23x <966890+Murphy23x@users.noreply.github.com>
2023-09-18 16:37:15 +0200

    Update Win11Pro-us.ps1

--- Scriptpad/Win11Pro-us.ps1
+++ Scriptpad/Win11Pro-us.ps1
@@ -8,7 +8,26 @@ Write-Host -ForegroundColor Green "Importing OSD Powershell Module"
 Import-Module OSD -Force
 
 Write-Host -ForegroundColor Green "Start OSD Cloud"
-Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 22H2 x64' -OSLicense Retail -OSEdition Pro -Zti
+Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 22H2 x64' -OSLicense Retail -OSEdition Pro
+
+#================================================
+#   WinPE PostOS Sample
+#   AutopilotOOBE Offline Staging
+#================================================
+Install-Module AutopilotOOBE -Force
+Import-Module AutopilotOOBE -Force
+
+$Params = @{
+    Title = 'OSDeploy Autopilot Registration'
+    GroupTag = 'Enterprise'
+    GroupTagOptions = 'Development','Enterprise'
+    Hidden = 'AddToGroup','AssignedComputerName','AssignedUser','PostAction'
+    Assign = $true
+    Run = 'NetworkingWireless'
+    Docs = 'https://autopilotoobe.osdeploy.com/'
+}
+AutopilotOOBE @Params
+
 
 Write-Host -ForegroundColor Green "Restarting ..."
 start-sleep -Seconds 5
