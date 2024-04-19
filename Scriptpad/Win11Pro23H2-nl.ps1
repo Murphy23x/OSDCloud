a4c9d2b1fb0fc9b29b20cc8febe4281769fe22dd
Murphy23x <966890+Murphy23x@users.noreply.github.com>
2024-04-19 12:00:58 +0200

    Update Win11Pro23H2-nl.ps1

--- Scriptpad/Win11Pro23H2-nl.ps1
+++ Scriptpad/Win11Pro23H2-nl.ps1
@@ -1,4 +1,4 @@
-Write-Host -ForegroundColor Green "Starting OSDCloud ZTI ..."
+Write-Host -ForegroundColor Green "Starting OSDCloud..."
 start-sleep -Seconds 3
 
 Write-Host -ForegroundColor Green "Updating OSD Powershell Module"
